8d695c611622f407f50cb3f1449489f6ecf11cbb
pontos2292 <58387973+pontos2292@users.noreply.github.com>
2022-12-12 11:38:06 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries']) #passed avocado and strawberries as examples
-fuits_to_show = my_fruit_list.loc[fruits_selected] #displays data frame exclusively for the fruits_selected
+fruits_to_show = my_fruit_list.loc[fruits_selected] #displays data frame exclusively for the fruits_selected
 
 streamlit.dataframe(fruits_to_show) #streamlit to present the dataframe pulled from S3 by pandas
 
