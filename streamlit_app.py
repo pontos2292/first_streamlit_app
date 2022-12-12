f14e8ee672f52890ef0b5ca16a9fb6fbc006cbc7
pontos2292 <58387973+pontos2292@users.noreply.github.com>
2022-12-12 11:14:31 +0100

    Update streamlit_app.py
    imported csv with pandas
    displayed with streamlit.dataframe()

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("My Parent's New Healthy Diner")
 streamlit.header("Breakfast Favorites")
@@ -8,3 +9,6 @@ streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text("🥑🍞 Avocado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt") #pandas to import csv with macros from AWS S3
+streamlit.dataframe(my_fruit_list) #streamlit to present the dataframe pulled from S3 by pandas
+
